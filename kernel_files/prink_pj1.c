66bfc6bad5e343332ed734824e3006d1d73a0651
yuyuwu5 <b06902104@ntu.edu.tw>
2020-04-27 10:57:48 +0800

    add kernel file

---
+++ kernel_files/prink_pj1.c
@@ -0,0 +1,9 @@
+#include <linux/linkage.h>
+#include <linux/kernel.h>
+
+//syscall 334
+
+asmlinkage void prink_pj1(int pid, long start, long end) {
+	static const long BASE = 1000000000;
+	printk("[Project1] %d %ld.%09ld %ld.%09ld", pid, start/BASE, start%BASE, end/BASE, end%BASE);
+}
---
+++ kernel_files/time_pj1.c
@@ -0,0 +1,11 @@
+#include <linux/linkage.h>
+#include <linux/kernel.h>
+
+// syscall 333
+
+asmlinkage long time_pj1(){
+		static const long BASE = 1000000000;
+		struct timespec t;
+		getnstimeofday(&t);
+		return t.tv_sec * BASE + t.tv_nsec;
+}	
